662b0cfd797098628eeeacba0b1db5e9e2caee05
Jonathon Hughes <>
2022-04-05 15:09:37 +0100

    config mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbain : {
+      url: process.env.MUMBAI_TEST_URL,
+      accounts : [process.env.ACCOUNT],
+      gas : 15e6
+    }
+  } 
 };
